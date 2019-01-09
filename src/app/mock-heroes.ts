72f1ab3d1e58c4aba2ba38e984499c7592b622e7
lulubun <laurapetitt@gmail.com>
2019-01-09 16:55:00 -0500

    I guess git is already initialized in an angular app

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Wolverine' },
+  { id: 12, name: 'Professor X' },
+  { id: 13, name: 'Storm' },
+  { id: 14, name: 'Rogue' },
+  { id: 15, name: 'Gambit' },
+  { id: 16, name: 'Cyclops' },
+  { id: 17, name: 'Jubilee' },
+  { id: 18, name: 'Magneto' },
+  { id: 19, name: 'Jean Gray' },
+  { id: 20, name: 'Mystique' }
+];
